f75f66c5fa080a0545daba5e5745c99e0051d928
nicomedinam7 <jhmedinalx3@gmail.com>
2018-11-29 16:59:34 -0500

    hola

--- app/src/main/java/com/example/nicolas/spinner/MainActivity.java
+++ app/src/main/java/com/example/nicolas/spinner/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         String [] opciones = {"sumar","restar","multiplicar","dividir"};
 
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,opciones);
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item_geekipedia,opciones);
         spinner1.setAdapter(adapter);
     }
 
